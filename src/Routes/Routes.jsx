3a081af524678dba7c28067235ebe31bcf4718e2
mugdhadey04 <mugdhadey8@gmail.com>
2025-04-17 10:01:56 +0600

    created the basic layout

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,22 @@
 import {
     createBrowserRouter,
     RouterProvider,
-  } from "react-router";
+} from "react-router";
+import Root from "../Pages/Root";
+import Error from "../Pages/ErrorPage/Error";
+import Home from "../Pages/Home/Home";
 
 export const router = createBrowserRouter([
     {
-      path: "/",
-      element: <div>Hello World</div>,
+        path: "/",
+        Component: Root,
+        errorElement: <Error></Error>,
+        children: [
+            {
+                index: true,
+                path: "/",
+                Component:Home
+            }
+        ]
     },
-  ]);
+]);
